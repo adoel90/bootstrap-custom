e8530419e3c65c65a4abae9ec50e46d4c2bcebc1
Adoel Rachman <adoel.cs@gmail.com>
2018-12-23 12:52:59 +0700

    Adding first time

---
+++ README.md
@@ -0,0 +1,3 @@
+# Goal : Can custom Bootstrap with sass and bundle those into bundle.js
+
+    - 
---
+++ src/app.js
@@ -0,0 +1,9 @@
+//https://stevenwestmoreland.com/2018/01/how-to-include-bootstrap-in-your-project-with-webpack.html
+
+import 'bootstrap';
+
+//Alternatively
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';
+
+import './scss/app.scss';
